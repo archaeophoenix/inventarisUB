ea3bf9045391e84fccfe53b0c24c139a8081b86e
archaeophoenix <aleppo.coy@gmail.com>
2016-09-25 09:42:11 +0700

    tambah keterangan

--- plugins/custom/custom.js
+++ plugins/custom/custom.js
@@ -1,64 +1,99 @@
+console.log($("#base").val()+' value dari $("#base").val();');
+
 function cetak(){
   var url = $("#base").val();
+  //fungsi print report
   $('.print').printElement({ leaveOpen: true, printMode: 'popup'});
 }
 
 function vendor(id){
+  //mengambil value dari atribut "rel" di pilihan option
   var id_vendor = $('#hrg'+id+' option:selected').attr("rel");
+  //set value yang sudah dipilih
   $('#vendor'+id).val(id_vendor);
+
+  console.log(id_vendor+' $("#hrg"+id+" option:selected").attr("rel");');
 }
 
 function periode(){
   var url = $("#base").val();
+  //ambil data tahun
   var tahun = $("#tahun").val();
+  //ambil data bulan
+  //ambil data bulan
   var bulan = $("#bulan").val();
+  //ambil data jenis
   var jenis = $("#jenis").val();
+  //ambil parameter
   var param = $("#param").val();
+
+  //direct ke halaman sesuai parameter
   window.location = url+''+param+'/'+bulan+'/'+tahun+'/'+jenis; //Relative or absolute path to response.php file
 }
 
 function edoirep(){
   var url = $("#base").val();
+  //ambil data tahun
   var tahun = $("#tahun").val();
+  //ambil data bulan
   var bulan = $("#bulan").val();
+  //ambil parameter
   var param = $("#param").val();
+  //direct ke halaman sesuai parameter
   window.location = url+''+param+'/'+bulan+'/'+tahun; //Relative or absolute path to response.php file
 }
 
 function supplier(){
   var url = $("#base").val();
+  //ambil data edit
   var edit = $("#edit").val();
+  //ambil data tahun
   var tahun = $("#tahun").val();
+  //ambil data bulan
   var bulan = $("#bulan").val();
+  //ambil data jenis
   var jenis = $("#jenis").val();
   var param = '';
+  
   if (jenis != 0) {param = jenis;}
+
+  //direct ke halaman sesuai parameter
   window.location = url+'report/supplier/'+edit+'/'+tahun+'/'+bulan+'/'+param; //Relative or absolute path to response.php file
 }
 
 function id_barang(id){
+  //ambil data dari atribut id di field
   var id_barang = $('.id_barang'+id).attr('id');
+  //set data dari atribut id di field
   $('#id_barang'+id).val(id_barang);
 }
 
 function modal(id){
+  //ambil data keterangan
   var keterangan = $('#keterangan'+id).val();
+  //ambil data path file
   var file = $('#file'+id).val();
+  //set data path file ke <img src>
   $('#img').attr('src',file);
+  //set data #txt
   $('#txt').text(keterangan);
-  console.log(keterangan);
 }
 
 function kinerja(kategori,bobot,nilai){
+  //set value berdasarkan hitungan
   var value = nilai*(bobot/100);
   var total = 0;
+  //set value dua angka dibelakang koma
   $('#'+kategori).text(value.toFixed(2));
+  //perulangan data array
   $(".total").each(function(index) {
+  //penjumblahan data array
     total += parseFloat($( this ).text());
-    // console.log( index + ": " + parseFloat($( this ).text()) );
   });
+  //set value dua angka dibelakang koma
   $('#hasil').text(total.toFixed(2));
   var grade = '';
+  //set grade berdasarkan hasil penjumblahan
   switch(true) {
     case (total <= 1): grade = 'D';
     break;
@@ -71,16 +106,24 @@ function kinerja(kategori,bobot,nilai){
     default : grade = '';
     break;
   }
+  // set grade
   $('#grade').text(grade);
 }
 
 $(function () {
 
+  //set element title dengan value text yang ada di element h1
   $('title').text('UB - '+$('h1').text());
+
+  //perkondisian, jika sesuai, maka keluar chart
   if($('#param').val() == 'report/inventaris'){
+    //set bagian chart
     var masuk = $("#masuk").val();
     var keluar = $("#keluar").val();
+    
+    //ambil object dari chart
     var donut = new Morris.Donut({
+      //set properti
       element: 'sales-chart',
       resize: true,
       colors: ["#3c8dbc", "#f56954"],
@@ -92,23 +135,34 @@ $(function () {
     });
   }
 
+  //get atribut dari class evaluasi yang sudah dipilih
   var ave = $('.evaluasi:checked').val();
+  //perkondisian
   if(ave == 3){
+    //kondidi menghilangkan disable jika sesuai
     $('#syarat').removeAttr('disabled');
   } else {
+    //kondidi disable jika tidak sesuai
     $('#syarat').attr('disabled','disabled');
   }
 
+  //perulangan value dari class total
   $(".total").each(function( index ) {
+    //set value dari atribut id class tersebut
     var kate = $(this).attr('id');
+    //atribut id class dengan kondisi cek akan di klik
     $("."+kate+":checked").click();
   });
 
+  //get atribut dari class evaluasi yang sudah dipilih
   $('.evaluasi').click(function(){
+    //perkondisian
     var eva = $(this).val();
     if(eva == 3){
+      //kondidi menghilangkan disable jika sesuai
       $('#syarat').removeAttr('disabled');
     } else {
+      //kondidi disable jika tidak sesuai
       $('#syarat').attr('disabled','disabled');
     }
   });
@@ -132,6 +186,7 @@ $(function () {
       "columns": JSON.parse($("#column").val())
   } );*/
 
+  //boleh dihapus
   $('.ntb').mouseout(function(){
     var text = $(this).attr('title');
     var atr = (text == "Belum Dianggarkan") ? "btn-info" : "btn-danger" ;
@@ -146,6 +201,7 @@ $(function () {
     $(this).attr('title',txt);
   });
 
+  //boleh dihapus
   $('.ntb').mouseover(function(){
     var text = $(this).attr('title');
     var atr = (text == "Belum Dianggarkan") ? "btn-info" : "btn-danger" ;
@@ -160,6 +216,7 @@ $(function () {
     $(this).attr('title',txt);
   });
 
+  //tambah atribut select2
   $(".select2").select2();
 
 /*  $(".select2").change(function() {
@@ -173,8 +230,11 @@ $(function () {
 
   $(".textarea").wysihtml5();
 
+  
+  //tambah atribut datatable
   $(".list1").DataTable();
 
+  //set atribut datatable
   $('#example2').DataTable({
     "paging": true,
     "lengthChange": false,
@@ -186,6 +246,8 @@ $(function () {
 
   // $('title').html("Inventaris | "+$('h1').text());
 
+  
+  //set atribut datepicker
   $(".datepicker").daterangepicker({
         singleDatePicker: true,
         showDropdowns: true,
@@ -193,14 +255,18 @@ $(function () {
     /*},function(start, end, label) {var years = moment().diff(start, 'years'); alert("You are " + years + " years old.");*/
   });
 
-  console.log($("#param").val());
+  console.log($("#param").val()+' value dari $("#param").val()');
 
+
+  //set atribut autocomplete
   $(".autocomplete").autocomplete({
+    //get value berdasarkan parameter
     source: $("#base").val()+'index/barang/'+$("#param").val(),
     select: function (event, ui) {
       event.preventDefault();
       $(this).val(ui.item.label);
       var c = $(this).attr('id');
+      //set value berdasarkan parameter
       $('#id'+c).val(ui.item.id);
       $('#merk'+c).text(ui.item.merk);
       $('#spec'+c).text(ui.item.spesifikasi);
@@ -212,6 +278,7 @@ $(function () {
       event.preventDefault();
       $(this).val(ui.item.label);
       var c = $(this).attr('id');
+      //set value berdasarkan parameter
       $('#id'+c).val(ui.item.id);
       $('#merk'+c).text(ui.item.merk);
       $('#spec'+c).text(ui.item.spesifikasi);
@@ -223,25 +290,35 @@ $(function () {
 });
 
 function tambah(){
+  //get value dengan id baris
   var baris = $('#baris').val();
+  //menjumlahkan baris
   baris++;
+  //set value dengan id baris
   $('#baris').val(baris);
+  // get object Date()
   var d = new Date();
+  // set date
   var gen = d.getDate()+''+d.getHours()+''+d.getMinutes()+''+d.getSeconds()+''+d.getMilliseconds();
   var tag = '';
+  //set value tag jika kondisi sesuai
   if ($("#inventaris").val() == 'masuk') {
     tag = '<tr id="baris'+gen+'"><td><input required="required" type="text" id="'+gen+'" value="" placeholder="Mac Book Air" class="form-control autocomplete"><input type="hidden" id="id'+gen+'" name="id_barang[]" value="" class="form-control"></td><td><p id="merk'+gen+'">&nbsp;</p></td><td><input required="required" type="number" value="" pattern="[0-9]" name="harga[]" placeholder="1000" class="form-control"></td><td><input required="required" type="text" value="" name="reg[]" placeholder="Label" class="form-control"></td><td><input required="required" type="number" value="" pattern="[0-9]" name="jumlah[]" placeholder="1000" class="form-control"></td><td><input required="required" type="text" value="" name="alokasi[]" placeholder="Alokasi" class="form-control"></td><td><input required="required" type="text" value="" name="keterangan[]" placeholder="Keterangan" class="form-control"></td><td><p id="op'+gen+'"><button type="button" onclick="minus('+gen+')" class="btn btn-danger btn-xs btn-circle hapus" title="Hapus"><i class="fa fa-trash"></i></button></p></td></tr>';
   } else {
     tag = '<tr id="baris'+gen+'"><td><input required="required" type="text" id="'+gen+'" value="" placeholder="Mac Book Air" class="form-control autocomplete"><input type="hidden" id="id'+gen+'" name="id_barang[]" value="" class="form-control"></td><td><p id="merk'+gen+'">&nbsp;</p></td><td><p id="sisa'+gen+'" class="text-right">&nbsp;</p></td></td><td><input required="required" type="number" value="" pattern="[0-9]" name="jumlah[]" placeholder="1000" class="form-control"></td><td><input required="required" type="text" value="" name="alokasi[]" placeholder="Alokasi" class="form-control"></td><td><input required="required" type="text" value="" name="keterangan[]" placeholder="Keterangan" class="form-control"></td><td><p id="op'+gen+'"><button type="button" onclick="minus('+gen+')" class="btn btn-danger btn-xs btn-circle hapus" title="Hapus"><i class="fa fa-trash"></i></button></p></td></tr>';
   }
+  //menambahkan value tag di id purchase
   $('#purchase').append(tag);
   console.log($(".autocomplete"));
+  //set atribut autocomplete
   $(".autocomplete").autocomplete({
+    //get value berdasarkan parameter
     source: $("#base").val()+'index/barang/'+$("#param").val(),
     select: function (event, ui) {
       event.preventDefault();
       $(this).val(ui.item.label);
       var c = $(this).attr('id');
+      //set value berdasarkan parameter
       $('#id'+c).val(ui.item.id);
       $('#merk'+c).text(ui.item.merk);
       $('#spec'+c).text(ui.item.spesifikasi);
@@ -253,6 +330,7 @@ function tambah(){
       event.preventDefault();
       $(this).val(ui.item.label);
       var c = $(this).attr('id');
+      //set value berdasarkan parameter
       $('#id'+c).val(ui.item.id);
       $('#merk'+c).text(ui.item.merk);
       $('#spec'+c).text(ui.item.spesifikasi);
@@ -264,19 +342,28 @@ function tambah(){
 }
 
 function plus() {
+  //get value dengan id baris
   var baris = $('#baris').val();
+  //menjumlahkan baris
   baris++;
+  //set value dengan id baris
   $('#baris').val(baris);
+  // get object Date()
   var d = new Date();
+  // set date
   var gen = d.getDate()+''+d.getHours()+''+d.getMinutes()+''+d.getSeconds()+''+d.getMilliseconds();
+  //menambahkan value di id purchase
   $('#purchase').append('<tr id="baris'+gen+'"><td><input required="required" type="text" id="'+gen+'" value="" placeholder="Mac Book Air" class="form-control autocomplete"><input type="hidden" id="id'+gen+'" name="id_barang[]" value="" placeholder="Mac Book Air" class="form-control"></td><td><input required="required" type="number" value="" pattern="[0-9]" name="jumlah[]" placeholder="10" class="form-control"></td><td><p id="merk'+gen+'"></p></td><td><p id="spec'+gen+'"></p></td><td><input required="required" type="number" value="" pattern="[0-9]" name="harga[]" placeholder="10" class="form-control"></td><td><p id="op'+gen+'"><button type="button" onclick="minus('+"'"+gen+"'"+');" class="btn btn-danger btn-xs btn-circle hapus" title="Hapus"><i class="fa fa-trash"></i></button></p></td></tr>');
   console.log($(".autocomplete"));
+  //set atribut autocomplete
   $(".autocomplete").autocomplete({
+    //get value berdasarkan parameter
     source: $("#base").val()+'index/barang/'+$("#param").val(),
     select: function (event, ui) {
       event.preventDefault();
       $(this).val(ui.item.label);
       var c = $(this).attr('id');
+      //set value berdasarkan parameter
       $('#id'+c).val(ui.item.id);
       $('#merk'+c).text(ui.item.merk);
       $('#spec'+c).text(ui.item.spesifikasi);
@@ -285,6 +372,7 @@ function plus() {
       event.preventDefault();
       $(this).val(ui.item.label);
       var c = $(this).attr('id');
+      //set value berdasarkan parameter
       $('#id'+c).val(ui.item.id);
       $('#merk'+c).text(ui.item.merk);
       $('#spec'+c).text(ui.item.spesifikasi);
@@ -293,11 +381,16 @@ function plus() {
 }
 
 function minus(id){
+  //get value dengan id baris
   var baris = parseInt($('#baris').val());
   console.log(baris);
+  //perkondisian
   if (baris > 1) {
+    //jika sesuai maka dikurangi
     baris--;
+    //set value baris
     $('#baris').val(baris);
+    //menghapus baris
     $('#baris'+id).remove();
   } else {
     alert('baris anda tinggal satu, anda tidak bisa menghapus baris lagi');
@@ -306,11 +399,14 @@ function minus(id){
 
 function inventaris(jenis){
   var base = $("#base").val();
+  //format data yang dipush
   var data = $('#inventaris').serializeArray();
   data.push({type: jenis});
+  //post data yang sudah di format
   $.post(base+'inventaris/form', data);
 }
 
+//boleh dihapus
 function status(id){
   $.ajax({
     type: "POST",
